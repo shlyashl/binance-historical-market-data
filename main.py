e831a144a52caeeb3cf89aac98d1b5b20e17b2fa
chubukovmn <ay-ya@ya.ru>
2022-12-22 20:34:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ from src.tools import ClickHouse
 from src.binance import Binance
 
 
-def main():
+def main(parse_date_start: str = None, parse_date_end: str = None):
     config = parse_config('config.yml')
+
+    if parse_date_start and parse_date_end:
+        config['binance']['parse_date_start'] = parse_date_start
+        config['binance']['parse_date_end'] = parse_date_end
 
     ch = ClickHouse(**config['clickhouse'])
 
-    bn_symbols_to_skip_query = ch.queries['bn_symbols_to_skip'].format(config['binance']['parse_date_start'],
-                                                                       config['binance']['parse_date_end'])
+    bn_symbols_to_skip_query = ch.queries['bn_symbols_to_skip'].format(
+        config['binance']['parse_date_start'],
+        config['binance']['parse_date_end'])
+
     bn_symbols_to_skip = ch.select(bn_symbols_to_skip_query).symbol.to_list()
 
     job = Binance(ch, bn_symbols_to_skip, **config['binance'])
